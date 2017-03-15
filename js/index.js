adf00cb73ba47811e2fa3d85b34d6f21278ebd6b
cdhartman <hartman.clint@mayo.edu>
2017-03-15 10:55:18 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -34,6 +34,9 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		var serial = device.serial;
+		var uuid = device.uuid;
+		document.getElementById("deviceInfo").innerHTML = "serial: " + serial + ", uuid: " + uuid;
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
